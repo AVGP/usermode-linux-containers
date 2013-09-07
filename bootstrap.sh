503e9ce54826d34ce0c0c2f7af6aa182e64f513b
Martin N. <mr.avgp@googlemail.com>
2013-09-07 18:34:33 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Installing dependencies..."
+apt-get install wget user-mode-linux uml-utilities bridge-utils debootstrap realpath
+
+echo "Setting up the tap0 network device..."
+echo "
+
+    auto tap0
+    iface tap0 inet static
+            address 10.1.0.1
+            netmask 255.255.255.0
+            tunctl_user uml-net
+" > /etc/network/interfaces
+
+echo "Adding the switched device to /etc/defaults/uml-utilities..."
+echo 'UML_SWITCH_OPTIONS="-tap tap0"' >> /etc/defaults/uml-utilities
+
+echo "Stopping UML daemon..."
+/etc/init.d/uml-utilities stop
+
+echo "Bringing up tap0 adapter..."
+ifup tap0
+
+echo "Starting UML daemon..."
+/etc/init.d/uml-utilities start
+
+echo "Done."
